49685c6a5ea97958f54e62308d296fbdd25f4827
Muhamad Ramadani <mrama311005@gmail.com>
2024-08-01 23:08:16 +0700

    feat: read data nilai

---
+++ app/Model/Nilai.php
@@ -0,0 +1,36 @@
+<?php
+
+class Nilai extends Database{
+    private $table = "nilai";
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function all(){
+        $data = $this->conn->query("SELECT nilai.*,m.nama_mhs,mtl.nama_matkul,s.nama_semester 
+                FROM nilai
+                INNER JOIN mahasiswa as m ON m.mahasiswa_id = nilai.mahasiswa_id
+                INNER JOIN matkul as mtl ON mtl.matkul_id = nilai.matkul_id
+                INNER JOIN semester as s ON s.semester_id = nilai.semester_id"
+        ) or die($this->conn->error);
+        return $data;
+    }
+
+    public function create($nilai,$nilai_akhir,$mahasiswa_id,$matkul_id,$semester_id){
+        return $this->conn->query("INSERT INTO $this->table (nilai,nilai_akhir,mahasiswa_id,semester_id) 
+        VALUES ('$nilai','$nilai_akhir','$mahasiswa_id','$matkul_id','$semester_id')");
+    }
+
+    public function getMhs(){
+        return $this->conn->query("SELECT m.mahasiswa_id,m.nim,m.nama_mhs FROM mahasiswa as m");
+    }
+
+    public function getMatkul(){
+        return $this->conn->query("SELECT * FROM matkul");
+    }
+    
+    public function getSemester(){
+        return $this->conn->query("SELECT * FROM semester");
+    }
+}
